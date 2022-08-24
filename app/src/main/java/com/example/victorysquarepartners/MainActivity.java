57968ac20735bb2dc2ebec24bf083458f1df6588
CatalinVanciu <73790145+CatalinVanciu@users.noreply.github.com>
2022-08-24 17:08:03 +0300

    extract data from the website

--- app/src/main/java/com/example/victorysquarepartners/MainActivity.java
+++ app/src/main/java/com/example/victorysquarepartners/MainActivity.java
@@ -3,12 +3,51 @@ package com.example.victorysquarepartners;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String URL = "http://universities.hipolabs.com/search?country/";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        retrieveData();
+
     }
+
+    private void retrieveData(){
+
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, URL, response -> {
+
+            try {
+                JSONArray jsonArray = new JSONArray(response);
+
+                for (int i=0;i<jsonArray.length();i++){
+                    Log.d("test", jsonArray.get(i).toString());
+                }
+
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+
+
+
+        }, error -> Log.d("VolleyError", error.toString()));
+
+        RequestQueue requestQueue = Volley.newRequestQueue(getApplicationContext());
+        requestQueue.add(stringRequest);
+
+    }
+
 }
--- app/src/main/java/com/example/victorysquarepartners/entities/Country.java
+++ app/src/main/java/com/example/victorysquarepartners/entities/Country.java
@@ -3,10 +3,10 @@ package com.example.victorysquarepartners.entities;
 import java.util.List;
 
 public class Country {
-    private List<Website> domains;
+    private List<String> domains;
     private String alphaTwoCode;
     private String country;
-    private List<Website> webPages;
+    private List<String> webPages;
     private String name;
     private String stateProvince;
 
@@ -14,7 +14,7 @@ public class Country {
 
     }
 
-    public Country(List<Website> domains, String alphaTwoCode, String country, List<Website> webPages, String name, String stateProvince){
+    public Country(List<String> domains, String alphaTwoCode, String country, List<String> webPages, String name, String stateProvince){
         this.domains = domains;
         this.alphaTwoCode = alphaTwoCode;
         this.country = country;
@@ -27,7 +27,7 @@ public class Country {
         }
     }
 
-    public List<Website> getDomains() {
+    public List<String> getDomains() {
         return domains;
     }
 
@@ -39,7 +39,7 @@ public class Country {
         return country;
     }
 
-    public List<Website> getWebPages() {
+    public List<String> getWebPages() {
         return webPages;
     }
 
